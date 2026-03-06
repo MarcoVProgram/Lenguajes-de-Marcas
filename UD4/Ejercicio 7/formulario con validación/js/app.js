984459cf41772c3846a08080317073cf39bb1e58
COMPUS021\daw1 <kevinraww2589@gmail.com>
2026-03-06 14:32:06 +0100

    Creación y compleción del ejercicio 7

--- UD4/Ejercicio 7/formulario con validación/js/app.js
+++ UD4/Ejercicio 7/formulario con validación/js/app.js
@@ -110,7 +110,7 @@ function validarFecha() {
     const f = leerTexto(fechaNacimiento);
     const fecha = new Date(f);
 
-    if (fecha - new Date() < 568024668000) {
+    if (new Date() - fecha < 568024668000) {
         mostrarError(errorFecha, "La fecha no es válida. Debes ser mayor de Edad.");
         return false;
     }
@@ -121,22 +121,74 @@ function validarFecha() {
 
 function validarProvincia() {
 
+    const p = leerTexto(provincia);
+    
+    if (p == '') {
+        mostrarError(errorProvincia, "Debes seleccionar una provincia.");
+        return false;
+    }
+
+    limpiarError(errorProvincia);
+    return true;
 };
 
 function validarPassword() {
 
+    const contra1 = leerTexto(password);
+    const minReq = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+
+    if (!contra1.match(minReq)) {
+        mostrarError(errorPassword, "La contraseña es insuficiente. Revisala e intentalo de nuevo.")
+        return false;
+    }
+    
+    limpiarError(errorPassword);
+    return true;
 };
 
 function validarPassword2() {
+    
+    if (!validarPassword()) {
+        mostrarError(errorPassword2, "No puedes copiar una contraseña no valida");
+        return false;
+    }
 
+    const contra2 = leerTexto(password2);
+    const contra1 = leerTexto(password);
+
+    if (!contra2.match(contra1)) {
+        mostrarError(errorPassword2, "Las contraseñas no coinciden.");
+        return false;
+    }
+
+    limpiarError(errorPassword2);
+    return true;
 };
 
 function validarObservaciones() {
 
+    const longitud = leerTexto(observaciones).length;
+
+    if (longitud > 200) {
+        mostrarError(errorObs, "Demasiados caracteres.");
+        return false;
+    }
+
+    limpiarError(errorObs);
+    return true;
 };
 
 function validarTerminos() {
 
+    var terms = terminos.checked;
+
+    if (terms == false) {
+        mostrarError(errorTerminos, "Debes aceptar los terminos y condiciones.");
+        return false;
+    }
+
+    limpiarError(errorTerminos);
+    return true;
 };
 
 //Funciones Interfaz
@@ -155,12 +207,130 @@ function actualizarEstadoGeneral(texto, correcto) {
 //Funciones Generales
 function validarFormulario() {
 
+    var validacion = generarResumenErrores();
+
+    if (validacion) {
+        estadoGeneral.style.color ='rgb(51, 160, 51)';
+    } else {
+        estadoGeneral.style.color ='rgb(194, 49, 49)';
+    }
+
+    return validacion;
 };
 
 function generarResumenErrores() {
 
+    let valid = [];
+    valid[0] = validarNombre();
+    valid[1] = validarApellidos();
+    valid[2] = validarEmail();
+    valid[3] = validarTelefono();
+    valid[4] = validarFecha();
+    valid[5] = validarProvincia();
+    valid[6] = validarPassword();
+    valid[7] = validarPassword2();
+    valid[8] = validarObservaciones();
+    valid[9] = validarTerminos();
+
+    let errores = "";
+    let allValid = true;
+
+    for (let i = 0; i < valid.length; i++) {
+
+        if (valid[i] === false) {
+
+            allValid = false;
+
+            switch (i) {
+                case 0: errores += '<li>Nombre Incorrecto</li>'; break;
+                case 1: errores += '<li>Apellido Incorrecto</li>'; break;
+                case 2: errores += '<li>Email Incorrecto</li>'; break;
+                case 3: errores += '<li>Teléfono Incorrecto</li>'; break;
+                case 4: errores += '<li>Fecha Incorrecta</li>'; break;
+                case 5: errores += '<li>Provincia Incorrecta</li>'; break;
+                case 6: errores += '<li>Contraseña Incorrecta</li>'; break;
+                case 7: errores += '<li>Las contraseñas no coinciden</li>'; break;
+                case 8: errores += '<li>Observaciones demasiado largas</li>'; break;
+                case 9: errores += '<li>Debes aceptar los términos</li>'; break;
+            }
+        }
+    }
+
+    if (allValid) {
+        errores = "<li>—</li>";
+    }
+
+    listaErrores.innerHTML = errores;
+
+    return allValid;
 };
 
 function limpiarFormulario() {
-
+    
+    limpiarError(errorNombre);
+    limpiarError(errorApellidos);
+    limpiarError(errorEmail);
+    limpiarError(errorTelefono);
+    limpiarError(errorFecha);
+    limpiarError(errorProvincia);
+    limpiarError(errorPassword);
+    limpiarError(errorPassword2);
+    limpiarError(errorObs);
+    limpiarError(errorTerminos);
+    
 };
+
+nombre.addEventListener('input', () => {
+    validarNombre();
+});
+
+apellidos.addEventListener('input', () => {
+    validarApellidos();
+});
+
+email.addEventListener('input', () => {
+    validarEmail();
+});
+
+telefono.addEventListener('input', () => {
+    validarTelefono();
+});
+
+fechaNacimiento.addEventListener('input', () => {
+    validarFecha();
+});
+
+provincia.addEventListener('input', () => {
+    validarProvincia();
+});
+
+password.addEventListener('input', () => {
+    validarPassword();
+});
+
+password2.addEventListener('input', () => {
+    validarPassword2();
+});
+
+observaciones.addEventListener('input', () => {
+    validarObservaciones();
+});
+
+terminos.addEventListener('input', () => {
+    validarTerminos();
+});
+
+btnReset.addEventListener('click', () => {
+    limpiarFormulario();
+});
+
+formRegistro.addEventListener('submit', (e) => {
+
+  e.preventDefault(); // Evita que se recargue la página
+
+    if (validarFormulario()) {
+        estadoTexto.textContent = "Formulario válido ✅";
+    } else {
+        estadoTexto.textContent = "Hay errores en el formulario, corrígelos e inténtalo nuevamente ❌";
+    }
+});
